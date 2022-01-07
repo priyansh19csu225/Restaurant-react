4472d889e3c572bf41d7f2b64330207e2a00fb71
priyansh gupta <priyansh19csu225@github.com>
2022-01-08 00:36:26 +0530

    react router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,31 +1,38 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
+// import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
-
+import { Route, Navigate , Routes } from 'react-router-dom';
 class Main extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-        dishes: DISHES,
-        selectedDish: null
+        dishes: DISHES
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
 
   render() {
+
+    const HomePage = () => {
+      return(
+          <Home />
+      );
+    }
+
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Routes>
+              <Route path='/home' element={<Home ></Home>} />
+              <Route exact path='/menu' element={<Menu dishes={this.state.dishes} />} />
+              {/* <Navigate to='/home' /> */}
+              <Route path='*' element={<Navigate replace to='/home' />} />
+        </Routes>
         <Footer />
       </div>
     );
