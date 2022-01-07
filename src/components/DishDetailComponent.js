f16c36427cde5f34b457e4122914debcee1f3c76
priyansh gupta <priyansh19csu225@github.com>
2022-01-07 19:42:40 +0530

    Presentational and Container Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,13 +1,8 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
+import { Card, CardImg,  CardText, CardBody,
     CardTitle } from 'reactstrap';
 
 class DishDetailComponent extends Component {
-
-    constructor(props) {
-        super(props);
-        
-    }
 
     renderDish(dish){
         if (dish != null){
@@ -66,12 +61,14 @@ class DishDetailComponent extends Component {
 
     render() {
         return(
+            <div className='container'>
             <div className="row">
             <div  className="col-12 col-md-5 m-1">
-            {this.renderDish(this.props.selectedDish)}
+            {this.renderDish(this.props.dish)}
             </div>
             <div  className="col-12 col-md-5 m-1">
-            {this.renderComments(this.props.selectedDish)}
+            {this.renderComments(this.props.dish)}
+            </div>
             </div>
             </div>
         );
