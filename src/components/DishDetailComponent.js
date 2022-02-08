5a146db1883894cfd23ab6e10686feb1c35f28a3
priyansh19csu225 <priyansh19csu225@ncuindia.edu>
2022-02-08 19:24:10 +0530

    Fetch Post Comment

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -27,7 +27,7 @@ class CommentForm extends Component {
    handleSubmit(values) {
       // console.log('Current State is: ' + JSON.stringify(values));
       // alert('Current State is: ' + JSON.stringify(values));
-      this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+      this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
       // event.preventDefault();
    }
 
@@ -117,7 +117,7 @@ function RenderDish({ dish }) {
    );
 }
 
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
    if (comments != null) {
       return (
          <div className="col-12 col-md-5 m-1">
@@ -132,7 +132,7 @@ function RenderComments({ comments, addComment, dishId }) {
                   );
                })}
             </ul>
-            <CommentForm dishId={dishId} addComment={addComment}></CommentForm>
+            <CommentForm dishId={dishId} postComment={postComment}></CommentForm>
          </div>
       );
    }
@@ -182,7 +182,7 @@ const DishDetail = (props) => {
 
                <RenderDish dish={props.dish} />
 
-               <RenderComments comments={props.comments}  addComment={props.addComment}
+               <RenderComments comments={props.comments}  postComment={props.postComment}
         dishId={props.dish.id}/>
 
 
