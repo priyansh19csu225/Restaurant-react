78996c54680183397f33dd0f43fafacb33c6ef42
priyansh19csu225 <priyansh19csu225@ncuindia.edu>
2022-02-08 20:17:04 +0530

    React Animation Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -5,6 +5,8 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
 
 
 const required = (val) => val && val.length;
@@ -106,6 +108,11 @@ class CommentForm extends Component {
 function RenderDish({ dish }) {
    return (
       <div className="col-12 col-md-5 m-1">
+         <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
          <Card>
             <CardImg top src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
@@ -113,6 +120,7 @@ function RenderDish({ dish }) {
                <CardText>{dish.description}</CardText>
             </CardBody>
          </Card>
+         </FadeTransform>
       </div>
    );
 }
@@ -123,14 +131,18 @@ function RenderComments({ comments, postComment, dishId }) {
          <div className="col-12 col-md-5 m-1">
             <h4>Comments</h4>
             <ul className="list-unstyled">
+            <Stagger in>
                {comments.map((comment) => {
                   return (
+                     <Fade in>
                      <li key={comment.id}>
                         <p>{comment.comment}</p>
                         <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                      </li>
+                     </Fade>
                   );
                })}
+               </Stagger>
             </ul>
             <CommentForm dishId={dishId} postComment={postComment}></CommentForm>
          </div>
