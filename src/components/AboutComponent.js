0bf9400caad5cb91d4a6c6190e4c619c1cfed20c
priyansh19csu225 <priyansh19csu225@ncuindia.edu>
2022-02-13 19:41:17 +0530

    week 4 assignment completed

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
+import { Fade, Stagger } from 'react-animation-components';
 function RenderLeader({ leader }) {
+    
+ 
     return (
+        <Fade in>
         <div key={leader.id} className="col-12 mt-3">
-            <Media tag="li">
-                <Media left middle>
-                    <Media object src={leader.image} alt={leader.name} />
-                </Media>
+            <Media tag="li">              
+                <Media left middle>       
+                    <Media object src={ baseUrl + leader.image} alt={leader.name} />
+                </Media>      
                 <Media body className="ml-5">
                     <Media heading>{leader.name}</Media>
                     <p>{leader.designation}</p>
                     <p>{leader.description}</p>
-                </Media>
+                </Media>   
             </Media>
         </div>
-
+        </Fade>
     );
 
 }
 
 function About(props) {
-
-    const leaders = props.leaders.map((leader) => {
+    
+    const leaders = props.leaders.leaders.map((leader) => {
         return (
             <RenderLeader leader={leader} />
         );
@@ -85,7 +91,9 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list>
+                        <Stagger in>
                         {leaders}
+                        </Stagger>
                     </Media>
                 </div>
             </div>
